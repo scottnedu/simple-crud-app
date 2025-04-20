6152fa517bb0dbc692e85b9e25a71288292b3793
Chinedu Oyioha <oyiohachinedujude@gmail.com>
2025-04-20 16:57:34 +0100

    my second commit

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 import express from 'express';
 const app = express();
 
+app.use(express.json()); // Middleware to parse JSON bodies
+app.use(express.urlencoded({ extended: true })); // Middleware to parse URL-encoded bodies
+
 // You can add body-parser, routes, etc. later
 
 app.get('/', (req, res) => {
---
+++ controllers/product.controller.js
@@ -0,0 +1,35 @@
+import Product from "../models/product.model";
+
+//create new product
+export const createProduct = async (req, res) => {
+    try {
+        const product = new Product(req.body);
+        await product.save();
+        res.status(201).json(product);
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
+};
+//get all products
+export const getProducts = async (req, res) => {
+    try {
+        const products = await Product.find();
+        res.status(200).json(products);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+//get product by id
+export const getProductById = async (req, res) => {
+    try {
+        const product = await Product.findById(req.params.id);
+        if (!product) {
+            return res.status(404).json({ message: "Product not found" });
+        }
+        res.status(200).json(product);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+//update product by id
+
